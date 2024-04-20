642d0c0fdc30eab00f3b1df0db5968a98445e526
Oliver Forral <intrepion@gmail.com>
2024-04-19 22:18:45 -0600

    First, adjust your script to include the nested query:

--- hello-prisma/script.ts
+++ hello-prisma/script.ts
@@ -3,8 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const user = await prisma.user.create({
+    data: {
+      name: "Bob",
+      email: "bob@prisma.io",
+      posts: {
+        create: {
+          title: "Hello World",
+        },
+      },
+    },
+  });
+  console.log(user);
 }
 
 main()
